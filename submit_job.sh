678a585e13d059b4e12147a595d5bdba15d098b8
janciszhang <80568017+janciszhang@users.noreply.github.com>
2024-09-10 15:04:48 +1000

    Add files via upload

--- submit_job.sh
+++ submit_job.sh
@@ -1,15 +1,22 @@
 #!/bin/bash
-#SBATCH --partition=cybersecurity
-#SBATCH --job-name=my_job
-#SBATCH --output=output.txt
-#SBATCH --error=error.txt
-#SBATCH --time=01:00:00
-#SBATCH --nodes=1
-#SBATCH --ntasks=1
-#SBATCH --gpus=1
+#SBATCH --job-name=gnn-metis     # Job name
+#SBATCH --output=gnn_metis_output.txt   # Standard output log file
+#SBATCH --error=gnn_metis_error.txt    # Standard error log file
+#SBATCH --partition=cybersecurity      # Partition to submit to
 
-# Load modules or activate conda environment if needed
-# module load ...
+#SBATCH --gres=gpu:1                   # Request 1 GPU (if available)
+#SBATCH --cpus-per-task=2              # Request 2 CPU cores
+#SBATCH --mem=4G                       # Request 4 GB of memory
 
-# Run your script
+#SBATCH --time=01:00:00                # Maximum time for the job (1 hour)
+#SBATCH --mail-type=ALL                # Send email on all events (begin, end, fail)
+#SBATCH --mail-user=s4069853@rmit.edu.au  # Your email address
+
+# Activate your virtual environment
+source /opt/home/s4069853/miniconda3/bin/activate
+
+# Change to the directory where your script is located
+cd /opt/home/s4069853/ESGNN/
+
+# Run your Python script
 python metis_calculation_job_GPU.py
